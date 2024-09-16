7495587c88a2508d480d39f59c9511a890bc8eb4
ThaboChauke <chaukethabo16@gmail.com>
2024-09-16 21:58:08 +0200

    fix: generating the right password length, design: move number display next to slider

--- javaScript-password_generator/script.js
+++ javaScript-password_generator/script.js
@@ -1,6 +1,3 @@
-let passwordLength = document.getElementById("inputSlider").value;
-let complexity = document.getElementById("complexity").value;
-let showPassword = document.getElementById("passBox");
 let numbers = "0123456789";
 let password = ""
 
@@ -9,37 +6,40 @@ const lowercase_alpha = "abcdefghijklmnopqrstuvwxyz";
 
 const delimiters = ",; \t|:-_/$\\~@#^&%*()[]{}";
 
-
 let low = capital_alpha + lowercase_alpha;
 let medium = lowercase_alpha + capital_alpha + numbers;
 let high = lowercase_alpha + capital_alpha + numbers + delimiters;
 
 
 function generatePassword() {
+    let passwordLength = parseInt(document.getElementById("inputSlider").value);
+    let complexity = document.getElementById("complexity").value;
+    let showPassword = document.getElementById("passBox");
+
     password = "";
 
     if (complexity === "LOW") {
-        for (let item = 0; item < passwordLength +  1; item++) {
-            let index = Math.floor(Math.random() * low.length)
-            password += low.charAt(index)
-            showPassword.value = password
+        for (let item = 0; item < passwordLength; item++) {
+            let index = Math.floor(Math.random() * low.length);
+            password += low.charAt(index);
         }
 
-    } else if (complexity === "MEDIUM" ) {
-        for (let item = 0; item < passwordLength + 1; item++) {
-            let index = Math.floor(Math.random() * medium.length)
-            password += medium.charAt(index)
+    } else if (complexity === "MEDIUM") {
+        for (let item = 0; item < passwordLength; item++) {
+            let index = Math.floor(Math.random() * medium.length);
+            password += medium.charAt(index);
         }
 
-    } else  {
-        for (let item = 0; item < passwordLength + 1; item++) {
-            let index = Math.floor(Math.random() * high.length)
-            password += high.charAt(index)
+    } else { 
+        for (let item = 0; item < passwordLength; item++) {
+            let index = Math.floor(Math.random() * high.length);
+            password += high.charAt(index);
         }
-
     }
-    // showPassword.value = password
-};
+
+    showPassword.value = password;
+}
+
 
 function updateSliderInput(val) {
     document.getElementById("textInput").value = val
